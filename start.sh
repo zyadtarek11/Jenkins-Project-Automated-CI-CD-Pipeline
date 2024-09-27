f3aee3783ec2c9bf93ad9e2d79d4efe5ef3534e4
zyadtarek11 <zyadtarek900@gmail.com>
2024-09-27 11:35:16 +0300

    panic2

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-kubectl config set-context --current --namesapce=jenkins
 kubectl apply -f jenkins-service.yaml
 kubectl apply -f jenkins-deployment.yaml
 kubectl create serviceaccount jenkins-service-account -n jenkins
 kubectl apply -f jenkins-cluster-role.yaml
 kubectl apply -f jenkins-cluster-role-binding.yaml
+kubectl apply -f jenkins-pv.yaml
