0acb8defb5dccd4e0cca82ca9fb87e45bc508c06
zyadtarek11 <zyadtarek900@gmail.com>
2024-10-05 19:44:25 +0300

    updated debug.sh

--- debug.sh
+++ debug.sh
@@ -1,6 +1,7 @@
 kubectl get deployment -n jenkins
 kubectl get pods -n jenkins
 kubectl get svc -n jenkins
+minikube service --all -n jenkins
 kubectl get pv -n jenkins
 kubectl get pvc -n jenkins
 
